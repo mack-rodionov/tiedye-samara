a25d1be7c7022730b335bcc69b1a70f6b7c9b969
Maksim Rodionov <75488276+mack-rodionov@users.noreply.github.com>
2022-06-03 11:44:18 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// Плавная прокрутка страницы
+$(document).ready(function() {
+	$('a[href*=#]').bind('click', function(e) {
+	e.preventDefault();
+ 
+	var target = $(this).attr("href");
+ 
+	$('html, body').stop().animate({ 
+		scrollTop: $(target).offset().top
+		}, 500, function() {
+			location.hash = target;
+		});
+ 
+		return false;
+	});
+});
